8762352033e616aaa28f8ef19af53cd47f733727
officialshxdy <vandenbusscheshady@gmail.com>
2025-01-23 11:23:47 +0100

    Initial Commit

---
+++ notesApp/notesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesAppApp.swift
+//  notesApp
+//
+//  Created by Shady Vandenbussche on 23/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct notesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
